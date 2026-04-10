ca61f415ef2a7a8df8f02a5c8c2ea495554ece18
吉天翔 <2154616325@qq.com>
2026-04-10 15:29:49 +0800

    第一次提交：上传我的所有初始代码

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+pluginManagement {
+    repositories {
+        // 阿里云全套镜像（包括公共库、Google库和Gradle插件库）
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") } // <-- 就是加了这行解决你的问题
+
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyFirstApp"
+include(":app")
